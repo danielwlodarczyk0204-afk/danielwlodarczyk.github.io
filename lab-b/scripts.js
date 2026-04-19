97de1fea8d823e0f62e026a0061113e616b3cd49
wd55774 <wd55774@zut.edu.pl>
2026-04-19 22:16:56 +0200

    dodana funkcja delete() i ogólnie zmiana logiki programu, drobne poprawki w pliku index.html (poprawione taki domykające input)

--- lab-b/scripts.js
+++ lab-b/scripts.js
@@ -1,9 +1,9 @@
 lista = document.getElementById("lista");
 wyszukiwarka = document.getElementById("wyszukiwarka");
-usuwanie =  document.getElementById("usuwanie");
 wpisywanie_tekstu = document.getElementById("wpisywanie_tekstu");
 wpisywanie_daty = document.getElementById("wpisywanie_daty");
 zapisywanie = document.getElementById("zapisywanie");
+
 
 class Todo {
   constructor(tasks){
@@ -12,39 +12,56 @@ class Todo {
   tasks = [];
 
   erase() {
-
-  }
-  draw() {
-    for (let i = 0; i < this.tasks.length; i++) {
-      let nowy_tekst = document.createElement("div");
-      nowy_tekst.className = "tekst";
-      nowy_tekst.textContent = this.tasks[i];
-
-      let nowa_data = document.createElement("input");
-      nowa_data.className = "data";
-      nowa_data.type = "date";
-
-      let nowe_usuwanie = document.createElement("input");
-      nowe_usuwanie.className = "usuwanie";
-      nowe_usuwanie.type = "button";
-
-      let nowy_element = document.createElement("li");
-      nowy_element.appendChild(nowy_tekst);
-      nowy_element.appendChild(nowa_data);
-      nowy_element.appendChild(nowe_usuwanie);
-      lista.appendChild(nowy_element);
+    while (lista.firstChild) {
+      lista.removeChild(lista.firstChild);
     }
   }
 
-  add(task) {
-    this.tasks.push(task);
+  delete(div_usuwanie) {
+    let task = div_usuwanie.parentElement;
+    let index = Array.from(lista.children).indexOf(task);
+    this.tasks.splice(index, 1);
+  }
+
+  draw() {
+    this.erase();
+    for (let i = 0; i < this.tasks.length; i++) {
+      lista.appendChild(this.tasks[i]);
+    }
+  }
+
+  add(tekst, data) {
+    let nowy_tekst = document.createElement("div");
+    nowy_tekst.className = "tekst";
+    nowy_tekst.textContent = tekst;
+
+    let nowa_data = document.createElement("input");
+    nowa_data.className = "data";
+    nowa_data.type = "date";
+    nowa_data.value = data
+
+    let nowe_usuwanie = document.createElement("input");
+    nowe_usuwanie.className = "usuwanie";
+    nowe_usuwanie.type = "button";
+    let tym = this
+
+    nowe_usuwanie.addEventListener("click", function() {
+      tym.delete(nowe_usuwanie)
+      tym.draw()
+    })
+
+    let nowy_element = document.createElement("li");
+    nowy_element.appendChild(nowy_tekst);
+    nowy_element.appendChild(nowa_data);
+    nowy_element.appendChild(nowe_usuwanie);
+    this.tasks.push(nowy_element)
   }
 }
 
-let todo = new Todo(["zadanie 1", "zadanie 2", "zadanie 3"]);
-document.todo = todo
-todo.add("zadanie 4")
-todo.draw();
+let todo = new Todo([]);
+document.todo = todo;
 
-todo.add("zadanie 5")
-todo.draw();
+zapisywanie.addEventListener("click", function() {
+  todo.add(wpisywanie_tekstu.value, wpisywanie_daty.value);
+  todo.draw();
+})
