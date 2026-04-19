e30550b9c62d8fba3d978df2a260ccca121b95e9
wd55774 <wd55774@zut.edu.pl>
2026-04-19 16:02:17 +0200

    Zrobiony szkielet strony oraz możliwość wstawiania nowych elementów (bez klasy Todo)

--- lab-b/scripts.js
+++ lab-b/scripts.js
@@ -1,1 +1,39 @@
-console.debug("Hello world!");
+lista = document.getElementById("lista");
+wyszukiwarka = document.getElementById("wyszukiwarka");
+usuwanie =  document.getElementById("usuwanie");
+wpisywanie_tekstu = document.getElementById("wpisywanie_tekstu");
+wpisywanie_daty = document.getElementById("wpisywanie_daty");
+zapisywanie = document.getElementById("zapisywanie");
+
+l_pozycji = 0
+
+
+zapisywanie.addEventListener("click", function() {
+  l_pozycji++;
+  let nowy_tekst = document.createElement("div");
+  nowy_tekst.className = "tekst";
+  // nowy_tekst.id = "tekst" + {l_pozycji};
+  nowy_tekst.textContent = wpisywanie_tekstu.value;
+
+  let nowa_data = document.createElement("input");
+  nowa_data.className = "data";
+  // nowa_data.id = "data" + {l_pozycji};
+  nowa_data.type = "date";
+
+  let nowe_usuwanie = document.createElement("input");
+  nowe_usuwanie.className = "usuwanie";
+  // nowe_usuwanie.id = "usuwanie" + {l_pozycji};
+  nowe_usuwanie.type = "button";
+
+  let nowy_element = document.createElement("li");
+  nowy_element.appendChild(nowy_tekst);
+  nowy_element.appendChild(nowa_data);
+  nowy_element.appendChild(nowe_usuwanie);
+  lista.appendChild(nowy_element);
+
+
+})
+
+usuwanie.addEventListener("click", function() {
+  this.parentElement.removeChild(this.parentElement);
+})
