ca1da2c48784dd1a8975d2cbf19a5bb8603b5a24
wd55774 <wd55774@zut.edu.pl>
2026-05-04 14:11:59 +0200

    Poprawiona wyszukiwarka, poprawione nazewnictwo zmiennych i funkcji

--- lab-b/scripts.js
+++ lab-b/scripts.js
@@ -8,6 +8,8 @@ let czy_zaznaczone_data = -1;
 let poprzedni_tekst = "---";
 let poprzednia_data = "";
 
+// ---------------------------------------------
+
 class Rzecz {
   tekst;
   data;
@@ -17,30 +19,61 @@ class Rzecz {
   }
 }
 
+// ---------------------------------------------
+
 class Todo {
   tasks = [];
   term = "";
 
   filter() {
     let pasujace_indeksy = [];
-    for (let i = 0; i < this.tasks.length; i++) {
-      if (this.tasks[i].tekst.includes(this.term)) {
+    if (this.term.length < 2) {
+      for (let i = 0; i < this.tasks.length; i++) {
         pasujace_indeksy.push(i);
+      }
+    }
+    else {
+      for (let i = 0; i < this.tasks.length; i++) {
+        if (this.tasks[i].tekst.includes(this.term)) {
+          pasujace_indeksy.push(i);
+        }
       }
     }
     return pasujace_indeksy;
   }
 
   wczytaj_z_local_storage() {
-    this.tasks = JSON.parse(localStorage.getItem("tasks"));
+    if (localStorage.getItem("tasks") === null) {
+      this.tasks = [];
+    }
+    else {
+      this.tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
   }
 
   zapisz_do_local_storage() {
     localStorage.setItem("tasks", JSON.stringify(this.tasks));
   }
 
-  znajdz_indeks(element) {
-    return Array.from(lista.children).indexOf(element.parentElement);
+  znajdz_indeks_listy(elem_listy) {
+    return Array.from(lista.children).indexOf(elem_listy.parentElement);
+  }
+
+  indeks_listy_na_indeks_tablicy(indeks_listy) {
+    let pasujace_indeksy = this.filter();
+    return pasujace_indeksy[indeks_listy];
+  }
+
+  indeks_tablicy_na_element_listy(indeks_tablicy) {
+    let pasujace_indeksy = this.filter();
+    let indeks = 0;
+    for (let i = 0; i < pasujace_indeksy.length; i++) {
+      if (pasujace_indeksy[i] === indeks_tablicy) {
+        indeks = i;
+        break;
+      }
+    }
+    return indeks;
   }
 
   erase() {
@@ -49,40 +82,47 @@ class Todo {
     }
   }
 
-  delete(div_usuwanie) {
-    this.tasks.splice(this.znajdz_indeks(div_usuwanie), 1);
+  delete(elem_listy_usuwanie) {
+    let indeks_listy = this.znajdz_indeks_listy(elem_listy_usuwanie);
+    let indeks_tablicy = this.indeks_listy_na_indeks_tablicy(indeks_listy);
+    this.tasks.splice(indeks_tablicy, 1);
     this.zapisz_do_local_storage();
   }
 
-  odklikniecie_tekst(element) {
-    if (element.value.trim().length < 3) {
+  odklikniecie_tekst(elem_listy_tekst) {
+    if (elem_listy_tekst.value.trim().length < 3) {
       alert("Błąd: tekst musi mieć co najmniej 3 znaki!");
-      element.value = poprzedni_tekst;
+      elem_listy_tekst.value = poprzedni_tekst;
     }
-    else if (element.value.trim().length > 255) {
+    else if (elem_listy_tekst.value.trim().length > 255) {
       alert("Błąd: tekst musi mieć co najwyżej 255 znaków!");
-      element.value = poprzedni_tekst;
+      elem_listy_tekst.value = poprzedni_tekst;
     }
     let nowy_tekst = document.createElement("div");
     nowy_tekst.className = "tekst";
-    nowy_tekst.textContent = element.value;
-    this.tasks[this.znajdz_indeks(element)].tekst = element.value;
-    element.parentNode.replaceChild(nowy_tekst, element);
+    nowy_tekst.textContent = elem_listy_tekst.value;
+    let indeks_listy = this.znajdz_indeks_listy(elem_listy_tekst);
+    let indeks_tablicy = this.indeks_listy_na_indeks_tablicy(indeks_listy);
+    this.tasks[indeks_tablicy].tekst = elem_listy_tekst.value;
+    elem_listy_tekst.parentNode.replaceChild(nowy_tekst, elem_listy_tekst);
     czy_zaznaczone_tekst = -1;
   }
 
-  odklikniecie_data(element) {
-    if ((Date.parse(element.value) < new Date().setHours(0,0,0,0)) && (element.value !== "")) {
+  odklikniecie_data(elem_listy_data) {
+    if ((Date.parse(elem_listy_data.value) < new Date().setHours(0,0,0,0)) && (elem_listy_data.value !== "")) {
       alert("Błąd: data musi być pusta albo późniejsza niż aktualna!");
-      element.value = poprzednia_data;
+      elem_listy_data.value = poprzednia_data;
     }
-    this.tasks[this.znajdz_indeks(element)].data = element.value;
+    let indeks_listy = this.znajdz_indeks_listy(elem_listy_data);
+    let indeks_tablicy = this.indeks_listy_na_indeks_tablicy(indeks_listy);
+    this.tasks[indeks_tablicy].data = elem_listy_data.value;
     czy_zaznaczone_data = -1;
   }
 
   draw() {
     let pasujace_indeksy = this.filter();
     this.erase();
+
     for (let indeks of pasujace_indeksy) {
       let nowy_tekst = document.createElement("div");
       nowy_tekst.className = "tekst";
@@ -119,11 +159,20 @@ class Todo {
   }
 }
 
+// ---------------------------------------------
+
 let todo = new Todo();
 document.todo = todo;
 
 todo.wczytaj_z_local_storage();
 todo.draw();
+
+// ---------------------------------------------
+
+wyszukiwarka.addEventListener("input", function () {
+  todo.term = wyszukiwarka.value;
+  todo.draw();
+})
 
 zapisywanie.addEventListener("click", function() {
   let rzecz = new Rzecz(wpisywanie_tekstu.value, wpisywanie_daty.value);
@@ -146,23 +195,27 @@ window.addEventListener("click", function (event) {
   if (event.target.className === "data") {
     if (czy_zaznaczone_data === -1) {
       poprzednia_data = event.target.value;
-      czy_zaznaczone_data = todo.znajdz_indeks(event.target);
+      let indeks_listy = todo.znajdz_indeks_listy(event.target);
+      czy_zaznaczone_data = todo.indeks_listy_na_indeks_tablicy(indeks_listy);
     }
   }
   else if (event.target.className === "tekst") {
     if (czy_zaznaczone_tekst === -1) {
       poprzedni_tekst = event.target.textContent;
-      czy_zaznaczone_tekst = todo.znajdz_indeks(event.target);
+      let indeks_listy = todo.znajdz_indeks_listy(event.target)
+      czy_zaznaczone_tekst = todo.indeks_listy_na_indeks_tablicy(indeks_listy);
       let nowy_tekst_2 = document.createElement("input");
       nowy_tekst_2.className = "tekst";
       nowy_tekst_2.value = event.target.textContent;
       event.target.parentNode.replaceChild(nowy_tekst_2, event.target);
     }
     else {
-      let element = Array.from(lista.children)[czy_zaznaczone_tekst].firstChild;
-      if (event.target !== element) {
-        todo.odklikniecie_tekst(element);
-        czy_zaznaczone_tekst = todo.znajdz_indeks(event.target);
+      let indeks_listy = todo.znajdz_indeks_listy(event.target)
+      let elem_listy_tekst = Array.from(lista.children)[indeks_listy].firstChild;
+      if (event.target !== elem_listy_tekst) {
+        todo.odklikniecie_tekst(elem_listy_tekst);
+        let indeks_listy = todo.znajdz_indeks_listy(event.target)
+        czy_zaznaczone_tekst = todo.indeks_listy_na_indeks_tablicy(indeks_listy);
         let nowy_tekst_2 = document.createElement("input");
         nowy_tekst_2.className = "tekst";
         nowy_tekst_2.value = event.target.textContent;
@@ -173,19 +226,16 @@ window.addEventListener("click", function (event) {
   }
   else {
     if (czy_zaznaczone_tekst !== -1) {
-      let element = Array.from(lista.children)[czy_zaznaczone_tekst].firstChild;
-      todo.odklikniecie_tekst(element);
+      let indeks_listy = todo.indeks_tablicy_na_element_listy(czy_zaznaczone_tekst);
+      let elem_listy_tekst = Array.from(lista.children)[indeks_listy].firstChild;
+      todo.odklikniecie_tekst(elem_listy_tekst);
       todo.zapisz_do_local_storage();
     }
     if (czy_zaznaczone_data !== -1) {
-      let element = Array.from(lista.children)[czy_zaznaczone_data].firstChild.nextSibling;
-      todo.odklikniecie_data(element);
+      let indeks_listy = todo.indeks_tablicy_na_element_listy(czy_zaznaczone_data);
+      let elem_listy_data = Array.from(lista.children)[indeks_listy].firstChild.nextSibling;
+      todo.odklikniecie_data(elem_listy_data);
       todo.zapisz_do_local_storage();
     }
   }
 }, {capture: true})
-
-wyszukiwarka.addEventListener("input", function () {
-  todo.term = wyszukiwarka.value;
-  todo.draw();
-})
