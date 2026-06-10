ad968da73129755c039bfddde04b83e0904e1053
wd55774 <wd55774@zut.edu.pl>
2026-06-10 20:31:03 +0200

    Pokazywanie komunikatu, gdy obrazek jest ułożony poprawnie

--- lab-c/scripts.js
+++ lab-c/scripts.js
@@ -1,9 +1,11 @@
-// function sprawdz(kafelki, pola) {
-//   for (let i = 0; i < 16; i++) {
-//     kafelki.dataTransfer.getData('text');
-//
-//   }
-// }
+function sprawdz(pola) {
+  for (let i = 0; i < 16; i++) {
+    if (!(pola[i].dataset.poprawny_nr === pola[i].dataset.przechowywany_nr)) {
+      return 0;
+    }
+  }
+  alert("gratulacje!!!");
+}
 
 let div_kafelki = document.getElementById("kafelki")
 let div_pola = document.getElementById("pola")
@@ -11,6 +13,7 @@ let div_pola = document.getElementById("pola")
 for (let i = 0; i < 16; i++) {
   let kafelek = document.createElement("canvas")
   kafelek.className = "kafelek";
+  kafelek.dataset.nr = i.toString();
   kafelek.id = `kafelek${i}`;
   kafelek.width = 150;
   kafelek.height = 100;
@@ -22,11 +25,13 @@ for (let i = 0; i < 16; i++) {
   let pole = document.createElement("div")
   pole.className = "pole";
   pole.id = `pole${i}`;
+  pole.dataset.nr = i.toString()
   div_pola.append(pole);
 }
 
 let kafelki = document.querySelectorAll(".kafelek");
 let pola = document.querySelectorAll(".pole");
+let kolejnosc = [];
 
 let przycisk_lokalizacja = document.getElementById("przycisk_lokalizacja");
 let map = L.map('mapa').setView([53.430127, 14.564802], 18);
@@ -34,8 +39,6 @@ let map = L.map('mapa').setView([53.430127, 14.564802], 18);
 L.tileLayer.provider('Esri.WorldImagery').addTo(map);
 let marker = L.marker([53.430127, 14.564802]).addTo(map);
 marker.bindPopup("<strong>Hello!</strong><br>This is a popup.");
-
-let kolejnosc = [];
 
 document.getElementById("przycisk_zapisywanie_mapy").addEventListener("click", function() {
   leafletImage(map, function (err, canvas) {
@@ -51,6 +54,8 @@ document.getElementById("przycisk_zapisywanie_mapy").addEventListener("click", f
 
     for (let i = 0; i < 16; i++) {
       let kafelek = kafelki[kolejnosc[i]];
+      let pole = pola[i];
+      pole.dataset.poprawny_nr = kolejnosc[i].toString();
       kafelek.draggable = true;
       let kaf_context = kafelek.getContext("2d");
       kaf_context.drawImage(canvas, 150 * (i % 4) , 100 * Math.floor(i / 4), 150, 100, 0, 0, 150, 100)
@@ -75,35 +80,21 @@ przycisk_lokalizacja.addEventListener("click", function (){
 
 for (let kafelek of kafelki) {
   kafelek.addEventListener("dragstart", function(event) {
-    // this.style.border = "2px dashed #D8D8FF";
     event.dataTransfer.setData("text", this.id);
-  });
-
-  kafelek.addEventListener("dragend", function(event) {
-    this.style.borderWidth = "0";
   });
 }
 
-let liczba_prawidlowych = 0;
 for (let i = 0; i < 16; i++) {
-  pola[i].addEventListener("dragenter", function (event) {
-    // this.style.border = "2px solid #7FE9D9";
-  });
-  pola[i].addEventListener("dragleave", function (event) {
-    // this.style.border = "2px dashed #7f7fe9";
-  });
   pola[i].addEventListener("dragover", function (event) {
     event.preventDefault();
   });
+
   pola[i].addEventListener("drop", function (event) {
     if (this.childElementCount === 0) {
-      let dane = event.dataTransfer.getData('text')
       let myElement = document.querySelector("#" + event.dataTransfer.getData('text'));
       this.appendChild(myElement);
-      this.style.border = "0px";
-      if (dane.includes(kolejnosc[i])) {
-        liczba_prawidlowych++;
-      }
+      this.dataset.przechowywany_nr = this.firstChild.dataset.nr;
+      sprawdz(pola);
     }
 
     // if (this.childElementCount === 1) {
