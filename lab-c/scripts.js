d36aeaaf9488604537a0b55abe4fcf06f9b14254
wd55774 <wd55774@zut.edu.pl>
2026-06-10 18:41:07 +0200

    Zmienienie tymczasowego wyglądu strony na docelowy (w tym uproszczenie marginów w celu rozwiązania problemów z układaniem elementów), zrobienie części mechanizmu drag and drop

--- lab-c/scripts.js
+++ lab-c/scripts.js
@@ -1,19 +1,27 @@
+// function sprawdz(kafelki, pola) {
+//   for (let i = 0; i < 16; i++) {
+//     kafelki.dataTransfer.getData('text');
+//
+//   }
+// }
+
 let div_kafelki = document.getElementById("kafelki")
 let div_pola = document.getElementById("pola")
 
 for (let i = 0; i < 16; i++) {
   let kafelek = document.createElement("canvas")
   kafelek.className = "kafelek";
-  kafelek.id = "kafelek${i}";
+  kafelek.id = `kafelek${i}`;
   kafelek.width = 150;
   kafelek.height = 100;
+  kafelek.draggable = false;
   div_kafelki.append(kafelek);
 }
 
 for (let i = 0; i < 16; i++) {
   let pole = document.createElement("div")
   pole.className = "pole";
-  pole.id = "pole${i}";
+  pole.id = `pole${i}`;
   div_pola.append(pole);
 }
 
@@ -27,10 +35,10 @@ L.tileLayer.provider('Esri.WorldImagery').addTo(map);
 let marker = L.marker([53.430127, 14.564802]).addTo(map);
 marker.bindPopup("<strong>Hello!</strong><br>This is a popup.");
 
+let kolejnosc = [];
+
 document.getElementById("przycisk_zapisywanie_mapy").addEventListener("click", function() {
   leafletImage(map, function (err, canvas) {
-    let kolejnosc = [];
-
     for (let i = 0; i < 16; i++) {
       let wylosowana_liczba = Math.floor(Math.random() * 16);
       if (!(kolejnosc.includes(wylosowana_liczba))) {
@@ -43,6 +51,7 @@ document.getElementById("przycisk_zapisywanie_mapy").addEventListener("click", f
 
     for (let i = 0; i < 16; i++) {
       let kafelek = kafelki[kolejnosc[i]];
+      kafelek.draggable = true;
       let kaf_context = kafelek.getContext("2d");
       kaf_context.drawImage(canvas, 150 * (i % 4) , 100 * Math.floor(i / 4), 150, 100, 0, 0, 150, 100)
     }
@@ -63,3 +72,48 @@ przycisk_lokalizacja.addEventListener("click", function (){
     enableHighAccuracy: false
   });
 })
+
+for (let kafelek of kafelki) {
+  kafelek.addEventListener("dragstart", function(event) {
+    // this.style.border = "2px dashed #D8D8FF";
+    event.dataTransfer.setData("text", this.id);
+  });
+
+  kafelek.addEventListener("dragend", function(event) {
+    this.style.borderWidth = "0";
+  });
+}
+
+let liczba_prawidlowych = 0;
+for (let i = 0; i < 16; i++) {
+  pola[i].addEventListener("dragenter", function (event) {
+    // this.style.border = "2px solid #7FE9D9";
+  });
+  pola[i].addEventListener("dragleave", function (event) {
+    // this.style.border = "2px dashed #7f7fe9";
+  });
+  pola[i].addEventListener("dragover", function (event) {
+    event.preventDefault();
+  });
+  pola[i].addEventListener("drop", function (event) {
+    if (this.childElementCount === 0) {
+      let dane = event.dataTransfer.getData('text')
+      let myElement = document.querySelector("#" + event.dataTransfer.getData('text'));
+      this.appendChild(myElement);
+      this.style.border = "0px";
+      if (dane.includes(kolejnosc[i])) {
+        liczba_prawidlowych++;
+      }
+    }
+
+    // if (this.childElementCount === 1) {
+    //   let myElement = document.querySelector("#" + event.dataTransfer.getData('text'));
+    //   let tym = myElement.dataTransfer.getData('text');
+    //   myElement.dataTransfer.setData('string', this.dataTransfer.getData('text'))
+    //
+    //   this.appendChild(myElement);
+    //   this.style.border = "2px dashed #7f7fe9";
+    // }
+
+  }, false);
+}
